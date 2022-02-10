3375252b799ad3b6d4caeb603a1fa4999125f77d
SOUP-404 <studiosoup404@gmail.com>
2022-02-09 19:10:56 -0500

    added docs and loops functoins

--- BankApplication/Program.cs
+++ BankApplication/Program.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿
+/*
+ * William Castle
+ * CIS 297
+ * Assigment 3
+ * Banking Application
+ * Febuary 9, 2022
+ * 
+ */
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +17,11 @@ using System.Threading.Tasks;
 
 namespace BankApplication
 {
+    /// <summary>
+    /// Account is base class to all other accounts and holds balance
+    /// has methods credit and depit for chaging value of balance
+    /// balance can never be below 0
+    /// </summary>
     public class Account
     {
         private decimal balance;
@@ -30,7 +46,7 @@ namespace BankApplication
         }
 
         //deposit money
-        public void credit(decimal x)
+        public virtual void credit(decimal x)
         {
             Console.WriteLine("Current Balance: $" + Balance);
             balance += x;
@@ -39,7 +55,7 @@ namespace BankApplication
         }
 
         //widthdraw money
-        public bool debit(decimal x)
+        public virtual bool debit(decimal x)
         {
             if ((balance - x) >= 0)
             {
@@ -60,6 +76,11 @@ namespace BankApplication
         }
     }
 
+    /// <summary>
+    /// Savings account is child of account and adds intrest
+    /// credit and debit are the same
+    /// calculating intrest returns the intrest due which it would then be credited to the balance
+    /// </summary>
     public class SavingsAccount : Account
     {
         private decimal intrest;
@@ -75,6 +96,11 @@ namespace BankApplication
         }
     }
 
+    /// <summary>
+    /// Checkings account adds fee which is applied whenever a transaction is made
+    /// fee is calculated with the transaction so balance will never go below 0
+    /// overrides credit and debit to add a fee to the method
+    /// </summary>
     public class CheckingAccount : Account
     {
         private decimal fee;
@@ -84,7 +110,7 @@ namespace BankApplication
             fee = f;
         }
 
-        public void credit(decimal x)
+        public override void credit(decimal x)
         {
             base.credit(x);
             Console.WriteLine("!Fee!");
@@ -93,7 +119,7 @@ namespace BankApplication
         }
 
         //widthdraw money
-        public bool debit(decimal x)
+        public override bool debit(decimal x)
         {
             if ((Balance - (x + fee)) >= 0)
             {
@@ -114,27 +140,137 @@ namespace BankApplication
 
     }
 
-
+    /// <summary>
+    /// driver of program, holds main looping function for app
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
             SavingsAccount savings = new SavingsAccount(1000.0m, 0.01m);
-
-            savings.credit(10);
-            savings.debit(10);
-            savings.debit(1000000);
-
-
             CheckingAccount checking = new CheckingAccount(1000.0m, 0.5m);
 
-            checking.credit(10);
-            checking.debit(10);
-            checking.debit(1000000);
+            var accounts = new List<Account>() { savings, checking };
 
-            
-            Console.WriteLine("END");
+            bool loop = true;
+            while (loop)
+            {
+                Console.WriteLine("Enter Command:");
+                Console.WriteLine("-------------------");
+                Console.WriteLine("1) Savings Account");
+                Console.WriteLine("2) Checkings Account");
+                Console.WriteLine("3) Proccess Accounts");
+                Console.WriteLine("4) Quit App");
+                Console.WriteLine("-------------------");
+                Console.Write("Command: ");
+                int senti = int.Parse(Console.ReadLine());
+                Console.Clear();
 
+                switch (senti)
+                {
+                    //savings
+                    case 1:
+                        bool loop_save = true;
+                        while (loop_save)
+                        {
+                            Console.WriteLine("-------------------");
+                            Console.WriteLine("SAVINGS ACCOUNT");
+                            Console.WriteLine("-------------------");
+                            Console.WriteLine("Enter Command:");
+                            Console.WriteLine("-------------------");
+                            Console.WriteLine("1) Deposit");
+                            Console.WriteLine("2) Widthdraw");
+                            Console.WriteLine("3) Back");
+                            Console.WriteLine("-------------------");
+                            int senti_save = int.Parse(Console.ReadLine());
+                            Console.Clear();
+
+                            switch (senti_save)
+                            {
+                                //Deposit
+                                case 1:
+                                    Console.WriteLine("Enter Ammount to Deposit");
+                                    Console.Write("$: ");
+                                    decimal d = decimal.Parse(Console.ReadLine());
+                                    accounts[0].credit(d);
+                                    break;
+                                //widthdraw
+                                case 2:
+                                    Console.WriteLine("Enter Ammount to Widthdraw");
+                                    Console.Write("$: ");
+                                    decimal w = decimal.Parse(Console.ReadLine());
+                                    accounts[0].debit(w);
+                                    break;
+                                //quit
+                                case 3:
+                                    loop_save = false;
+                                    break;
+                            }
+                        }
+                        break;
+                    //checkings
+                    case 2:
+                        bool loop_checking = true;
+                        while (loop_checking)
+                        {
+                            Console.WriteLine("-------------------");
+                            Console.WriteLine("CHECKINGS ACCOUNT");
+                            Console.WriteLine("-------------------");
+                            Console.WriteLine("Enter Command:");
+                            Console.WriteLine("-------------------");
+                            Console.WriteLine("1) Deposit");
+                            Console.WriteLine("2) Widthdraw");
+                            Console.WriteLine("3) Back");
+                            Console.WriteLine("-------------------");
+                            int senti_check = int.Parse(Console.ReadLine());
+                            Console.Clear();
+
+                            switch (senti_check)
+                            {
+                                //Deposit
+                                case 1:
+                                    Console.WriteLine("Enter Ammount to Deposit");
+                                    Console.Write("$: ");
+                                    decimal d = decimal.Parse(Console.ReadLine());
+                                    accounts[1].credit(d);
+                                    break;
+                                //widthdraw
+                                case 2:
+                                    Console.WriteLine("Enter Ammount to Widthdraw");
+                                    Console.Write("$: ");
+                                    decimal w = decimal.Parse(Console.ReadLine());
+                                    accounts[1].debit(w);
+                                    break;
+                                //quit
+                                case 3:
+                                    loop_checking = false;
+                                    break;
+                            }
+                        }
+                        break;
+                    //Process intrest
+                    case 3:
+                        foreach(var current in accounts)
+                        {
+                            if(current is SavingsAccount)
+                            {
+                                var save = (SavingsAccount)current;
+                                current.credit(save.calculateIntrest());
+
+                                Console.WriteLine("Savings Account: $" + save.Balance);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Checkings Account: $" + current.Balance);
+                            }
+                        }
+                        break;
+                    //quit
+                    case 4:
+                        loop = false;
+                        break;
+                }
+            }
         }
     }
 }
